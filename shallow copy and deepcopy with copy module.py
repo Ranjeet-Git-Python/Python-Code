ac896813ac3c51f625dce5e120bda61a42622a52
Ranjeet Kumar <ranjeetkpune@gmail.com>
2026-09-26 15:51:51 +0530

    renamed flatten question

--- shallow copy and deepcopy with copy module.py
+++ shallow copy and deepcopy with copy module.py
@@ -9,6 +9,7 @@ print(list_2)	# output => [1, 2, [3, 5, 12], 11]
 print(list_1)	# output => [1, 2, [3, 5, 12], 4]
 
 # deep
+list_1 = [1, 2, [3, 5, 6], 4]
 list_3 = deepcopy(list_1)
 list_3[3] = 10
 list_3[2].append(13)
